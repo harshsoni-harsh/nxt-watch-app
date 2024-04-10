1d3fc50e8919155b4c3e3c5745bd9dc97f45b830
harshsoni-harsh <52278293+harshsoni-harsh@users.noreply.github.com>
2024-04-10 11:44:21 +0530

    Updated layout for small devices

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,3 +1,9 @@
+import Cookies from "js-cookie";
+import { withRouter } from "react-router-dom";
+
+import { FiLogOut } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi";
+
 import ThemeContext from "../../context/ThemeContext";
 import {
   Image,
@@ -9,40 +15,56 @@ import {
 } from "./styledComponents";
 import SideNav from "../SideNav";
 
-const Layout = (props) => (
-  <ThemeContext.Consumer>
-    {(value) => (
-      <OuterContainer>
-        <NavContainer dark={value.dark}>
-          <Image
-            logo
-            src={
-              value.dark
-                ? "https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png"
-                : "https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
-            }
-          />
-          <InnerContainer>
-            <Button icon onClick={value.changeTheme}>
-              {value.dark ? (
-                <Image icon dark={value.dark} src="/icons/light-mode.svg" />
-              ) : (
-                <Image icon dark={value.dark} src="/icons/dark-mode.svg" />
-              )}
-            </Button>
-            <Image src="https://assets.ccbp.in/frontend/react-js/nxt-watch-profile-img.png" />
-            <Button logout onClick={value.logOut}>
-              Logout
-            </Button>
-          </InnerContainer>
-        </NavContainer>
-        <Body>
-          <SideNav />
-          {props.children}
-        </Body>
-      </OuterContainer>
-    )}
-  </ThemeContext.Consumer>
-);
+const Layout = (props) => {
+  const logout = () => {
+    const { history } = props;
+    Cookies.remove("jwt_token");
+    history.replace("/login");
+  };
+  return (
+    <ThemeContext.Consumer>
+      {(value) => (
+        <OuterContainer>
+          <NavContainer dark={value.dark}>
+            <Image
+              logo
+              src={
+                value.dark
+                  ? "https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png"
+                  : "https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+              }
+            />
+            <InnerContainer>
+              <Button icon onClick={value.changeTheme}>
+                {value.dark ? (
+                  <Image icon dark={value.dark} src="/icons/light-mode.svg" />
+                ) : (
+                  <Image icon dark={value.dark} src="/icons/dark-mode.svg" />
+                )}
+              </Button>
+              <Image
+                profile
+                src="https://assets.ccbp.in/frontend/react-js/nxt-watch-profile-img.png"
+              />
+              <Button hamburger>
+                <GiHamburgerMenu />
+              </Button>
+              <Button logout onClick={logout}>
+                Logout
+              </Button>
+              <Button logoutIcon onClick={logout}>
+                <FiLogOut />
+              </Button>
+            </InnerContainer>
+          </NavContainer>
+          <Body>
+            <SideNav />
+            {props.children}
+          </Body>
+        </OuterContainer>
+      )}
+    </ThemeContext.Consumer>
+  );
+};
 
-export default Layout;
+export default withRouter(Layout);
--- src/components/Layout/styledComponents.js
+++ src/components/Layout/styledComponents.js
@@ -1,22 +1,35 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Image = styled.img`
   height: 25px;
   pointer-events: none;
-  ${(props) => (props.logo ? "height: 40px;" : "")}
-  ${(props) =>
-    !props.icon ? "" : props.dark ? "filter: invert(1);" : ""}
+  ${(props) => (props.logo ? "height: min(40px, 100%); max-width: 8em;" : "")}
+  ${(props) => (!props.icon ? "" : props.dark ? "filter: invert(1);" : "")}
+  @media (max-width: 767px) {
+    ${(props) => (props.profile ? "display: none;" : "")}
+  }
 `;
 export const Button = styled.button`
+  background: transparent;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  border: none;
+  outline: none;
   ${(props) =>
-    props.icon
-      ? "background: transparent; cursor: pointer; padding: 0px; margin: 0; border: none; outline: none;"
-      : props.logout
-      ? "background: transparent; border: 2px solid #3b82f6; border-radius: 2px; color: #3b82f6; font-weight: bold; padding: 5px 10px; height: fit-content;"
+    props.logout
+      ? "border: 2px solid #3b82f6; border-radius: 2px; color: #3b82f6; font-weight: bold; padding: 5px 10px; height: fit-content;"
+      : props.logoutIcon || props.hamburger
+      ? "display: none; font-size: 24px;"
       : ""}
+  @media (max-width: 767px) {
+    ${(props) => (props.logout ? "display: none" : "")}
+    ${(props) => (props.logoutIcon || props.hamburger ? "display: block" : "")}
+  }
 `;
 export const NavContainer = styled.div`
   width: 100%;
+  flex-grow: 1;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -27,8 +40,15 @@ export const InnerContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 20px;
+  @media (max-width: 767px) {
+    gap: 12px;
+  }
 `;
-export const OuterContainer = styled.div``
+export const OuterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
 export const Body = styled.div`
   display: flex;
-`
+`;
